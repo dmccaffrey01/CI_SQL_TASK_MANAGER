490efc52765b4f27049f0d71cf525eece08f96bf
Dillon Mc Caffrey <dmccaffrey01@gmail.com>
2023-04-18 21:19:08 +0000

    Add tasks

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
+    $('.modal').modal();
+    $('.datepicker').datepicker({
+        format: "dd mmmm, yyy",
+        i18n: {done: "Select"}
+    });
+    $('select').formSelect();
 });
+
